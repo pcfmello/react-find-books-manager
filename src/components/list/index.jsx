bb87be8bba4ca19cf59a787662bd2bb734c994e3
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-10-17 21:31:27 -0300

    Implements pagination.

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,123 +1,129 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Pagination from "react-js-pagination";
+
 import './_styles.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAngleLeft, faAngleDoubleLeft, faAngleRight, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 
-const List = ({ list }) => (
-  <div className="list d-flex flex-column">
-    <table className="books-list">
-      <thead>
-        <tr>
-          <th className="title">Livro</th>
-          <th className="author">Autor</th>
-          <th className="company">Editora</th>
-          <th className="year text-right">Ano</th>
-          <th className="actions  text-center">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td className="title">
-            <div>O pequeno príncipe - Edição de bolso</div>
-            <div>(9788561706210)</div>
-          </td>
-          <td className="author">Mark</td>
-          <td className="company">Otto</td>
-          <td className="year text-right">2011</td>
-          <td className="actions text-center">
-            <a href="#">Detalhes</a>
-          </td>
-        </tr>
-        <tr>
-          <td className="title">
-            <div>O pequeno príncipe - Edição de bolso</div>
-            <div>(9788561706210)</div>
-          </td>
-          <td className="author">Mark</td>
-          <td className="company">Otto</td>
-          <td className="year text-right">2009</td>
-          <td className="actions text-center">
-            <a href="#">Detalhes</a>
-          </td>
-        </tr>
-        <tr>
-          <td className="title">
-            <div>O pequeno príncipe - Edição de bolso</div>
-            <div>(9788561706210)</div>
-          </td>
-          <td className="author">Mark</td>
-          <td className="company">Otto</td>
-          <td className="year text-right">2002</td>
-          <td className="actions text-center">
-            <a href="#">Detalhes</a>
-          </td>
-        </tr>
-        <tr>
-          <td className="title">
-            <div>O pequeno príncipe - Edição de bolso</div>
-            <div>(9788561706210)</div>
-          </td>
-          <td className="author">Mark</td>
-          <td className="company">Otto</td>
-          <td className="year text-right">2005</td>
-          <td className="actions  text-center">
-            <a href="#">Detalhes</a>
-          </td>
-        </tr>
-        <tr>
-          <td className="title">
-            <div>O pequeno príncipe - Edição de bolso</div>
-            <div>(9788561706210)</div>
-          </td>
-          <td className="author">Mark</td>
-          <td className="company">Otto</td>
-          <td className="year text-right">2016</td>
-          <td className="actions text-center">
-            <a href="#">Detalhes</a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+class List extends React.PureComponent {
 
-    <nav>
-      <ul className="pagination justify-content-center align-items-center">
-        <li className="page-item">
-          <a className="page-link" href="#">
-            <span aria-hidden="true"><FontAwesomeIcon className="calendar-icon" icon={faAngleDoubleLeft} /></span>
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            <span aria-hidden="true"><FontAwesomeIcon className="calendar-icon" icon={faAngleLeft} /></span>
-          </a>
-        </li>
+  constructor(props) {
+    super(props);
 
-        <li className="page-item"><a className="page-link" href="#">1</a></li>
-        <li className="page-item"><a className="page-link" href="#">2</a></li>
-        <li className="page-item"><a className="page-link" href="#">3</a></li>
-        <li className="page-item"><a className="page-link active" href="#">4</a></li>
-        <li className="page-item"><a className="page-link" href="#">5</a></li>
-        <li className="page-item"><a className="page-link" href="#">6</a></li>
-        <li className="page-item"><a className="page-link" href="#">...</a></li>
-        <li className="page-item"><a className="page-link" href="#">50</a></li>
+    this.state = {
+      activePage: 4
+    };
 
-        <li className="page-item">
-          <a className="page-link" href="#">
-            <span aria-hidden="true"><FontAwesomeIcon className="calendar-icon" icon={faAngleDoubleRight} /></span>
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            <span aria-hidden="true"><FontAwesomeIcon className="calendar-icon" icon={faAngleRight} /></span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+    this._handlePageChange = this._handlePageChange.bind(this);
+  }
+
+  _handlePageChange(pageNumber) {
+    console.log(`active page is ${pageNumber}`);
+    this.setState({activePage: pageNumber});
+  }
+
+  render() {
+
+    const { activePage } = this.state;
+
+    return (
+      <div className="list d-flex flex-column">
+        <table className="books-list">
+          <thead>
+            <tr>
+              <th className="title">Livro</th>
+              <th className="author">Autor</th>
+              <th className="company">Editora</th>
+              <th className="year text-right">Ano</th>
+              <th className="actions  text-center">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="title">
+                <div>O pequeno príncipe - Edição de bolso</div>
+                <div>(9788561706210)</div>
+              </td>
+              <td className="author">Mark</td>
+              <td className="company">Otto</td>
+              <td className="year text-right">2011</td>
+              <td className="actions text-center">
+                <a href="#">Detalhes</a>
+              </td>
+            </tr>
+            <tr>
+              <td className="title">
+                <div>O pequeno príncipe - Edição de bolso</div>
+                <div>(9788561706210)</div>
+              </td>
+              <td className="author">Mark</td>
+              <td className="company">Otto</td>
+              <td className="year text-right">2009</td>
+              <td className="actions text-center">
+                <a href="#">Detalhes</a>
+              </td>
+            </tr>
+            <tr>
+              <td className="title">
+                <div>O pequeno príncipe - Edição de bolso</div>
+                <div>(9788561706210)</div>
+              </td>
+              <td className="author">Mark</td>
+              <td className="company">Otto</td>
+              <td className="year text-right">2002</td>
+              <td className="actions text-center">
+                <a href="#">Detalhes</a>
+              </td>
+            </tr>
+            <tr>
+              <td className="title">
+                <div>O pequeno príncipe - Edição de bolso</div>
+                <div>(9788561706210)</div>
+              </td>
+              <td className="author">Mark</td>
+              <td className="company">Otto</td>
+              <td className="year text-right">2005</td>
+              <td className="actions  text-center">
+                <a href="#">Detalhes</a>
+              </td>
+            </tr>
+            <tr>
+              <td className="title">
+                <div>O pequeno príncipe - Edição de bolso</div>
+                <div>(9788561706210)</div>
+              </td>
+              <td className="author">Mark</td>
+              <td className="company">Otto</td>
+              <td className="year text-right">2016</td>
+              <td className="actions text-center">
+                <a href="#">Detalhes</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div>
+          <Pagination
+            activePage={activePage}
+            itemsCountPerPage={11}
+            totalItemsCount={450}
+            pageRangeDisplayed={6}
+            onChange={this._handlePageChange}
+            firstPageText={<FontAwesomeIcon icon={faAngleDoubleLeft} />}
+            prevPageText={<FontAwesomeIcon className="calendar-icon" icon={faAngleLeft} />}
+            nextPageText={<FontAwesomeIcon className="calendar-icon" icon={faAngleRight} />}
+            lastPageText={<FontAwesomeIcon icon={faAngleDoubleRight} />}
+
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
 
 List.propTypes = {
   list: PropTypes.array.isRequired
